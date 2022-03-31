fa79ba05f5b745482e5b5c2525a847efbecff0fd
kamil <xkamilwrobel@gmail.com>
2022-03-31 17:06:40 +0200

    add loggin config

--- backend/src/test/java/org/example/backend/UserRegisterTest.java
+++ backend/src/test/java/org/example/backend/UserRegisterTest.java
@@ -14,10 +14,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+@Execution(ExecutionMode.SAME_THREAD)
 @DisplayName("user: register")
 class UserRegisterTest extends BaseApiSuite {
 
